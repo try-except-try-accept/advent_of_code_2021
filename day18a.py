2344f42ba0d0dbf8b1c16abe0f7e1200ad2c0b71
Chris Hall <31258080+chall168421@users.noreply.github.com>
2021-12-18 18:09:01 +0700

    day 18 part a - check_depth() implemented

---
+++ day18a.py
@@ -0,0 +1,62 @@
+from re import search, match, findall
+from collections import Counter
+from helpers import PuzzleHelper
+
+PP_ARGS = False, False #rotate, cast int
+
+DAY = 18
+TEST_DELIM = "---"
+FILE_DELIM = "\n"
+TESTS = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]///4140"""
+
+DEBUG = True
+
+def check_depth(pair):
+    try:
+        for i in range(4):
+            pair = pair[0]
+        return True
+    except:
+        return False
+        
+
+
+def solve(data):
+
+
+    #To explode a pair, the pair's left value is added to the first
+
+    # regular number to the left of the exploding pair (if any), and
+    #the pair's right value is added to the first regular number to
+    #the right of the exploding pair (if any). Exploding pairs will
+    #always consist of two regular numbers. Then, the entire exploding
+    #pair is replaced with the regular number 0.for row in data:
+
+
+    pass
+    
+
+    
+    
+
+    return count
+
+
+
+
+if __name__ == "__main__":
+    p = PuzzleHelper(DAY, TEST_DELIM, FILE_DELIM, DEBUG, PP_ARGS)
+
+    if p.check(TESTS, solve):
+        puzzle_input = p.load_puzzle()
+        puzzle_input = p.pre_process(puzzle_input, *PP_ARGS)
+        print("FINAL ANSWER: ", solve(puzzle_input))
