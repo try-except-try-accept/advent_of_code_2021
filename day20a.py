d7147a08c7bb91a5e6c1462b5128d46177f96bf2
Chris Hall <31258080+chall168421@users.noreply.github.com>
2021-12-20 17:39:54 +0700

    day 20 part a - example passes, puzzle input no

--- day20a.py
+++ day20a.py
@@ -1,6 +1,7 @@
 from re import search, match, findall
 from collections import Counter
 from helpers import PuzzleHelper
+from copy import deepcopy
 
 PP_ARGS = False, False #rotate, cast int
 
@@ -26,11 +27,13 @@ DEBUG = True
 
 def get_square_bin(img, y, x):
     bina = ""
-    for y in range(y-1, y+2, 1):
-        for x in range(x-1, x+2, 1):
-            bina += img[y][x].replace("#", "1").replace(".", "0")
-            
-    return int(bina, 2)
+    
+    for i in range(y-1, y+2, 1):
+        for j in range(x-1, x+2, 1):
+            bina += img[i][j].replace("#", "1").replace(".", "0")
+       
+    result =  int(bina, 2)
+    return result
             
             
             
@@ -40,9 +43,10 @@ def solve(data):
     
     DARK = '.'
     LIGHT = "#"
-    BUFFER = 5 * DARK
-    input_img = [BUFFER + BUFFER + BUFFER] * 5
-    output_img = deepcopy(input_img)
+    BUFFER_SIZE = 103
+    BUFFER = BUFFER_SIZE * DARK
+    input_img = [list(BUFFER + BUFFER + BUFFER) for j in range(BUFFER_SIZE)]
+    
     end = False
     for line in data:
         if line == "X":
@@ -51,28 +55,33 @@ def solve(data):
         if not end:
             enhancement += line
         else:
-            input_img.append(BUFFER + line + BUFFER)
-    input_img += [BUFFER + BUFFER + BUFFER] * 5
-
-    
-    
-    for y, row in enumerate(input_img):
-        for x, pixel in enumerate(row):
-            try:
-                square_bin = get_square_bin(input_img, y, x)
-                print("at", x, y, ":", square_bin)
-            except:
-                
+            input_img.append(list(BUFFER + line + BUFFER))
         
+    input_img += [list(BUFFER + BUFFER + BUFFER) for j in range(BUFFER_SIZE)]
+    output_img = deepcopy(input_img)
 
-    
+    output_img_v2 = deepcopy(output_img)
 
-    
+    count = 0
 
-        
+    for enhance in range(2):
+        for y, row in enumerate(input_img):
+            for x, pixel in enumerate(row):
+                try:
+                    square_bin = get_square_bin(input_img, y, x)
+                    output_img[y][x] = enhancement[square_bin]
+                    
+                    if output_img[y][x] == "#" and enhance == 1:
+                        count += 1    
+                except:
+                    pass
 
-        
-    
+        for row in output_img:
+            print("".join(row))
+        input()
+             
+        input_img = deepcopy(output_img)
+        output_img = output_img_v2
 
     return count
 
@@ -80,6 +89,7 @@ def solve(data):
 
 
 if __name__ == "__main__":
+
     p = PuzzleHelper(DAY, TEST_DELIM, FILE_DELIM, DEBUG, PP_ARGS)
 
     if p.check(TESTS, solve):
