6b7ab9809a82d333332763b8dc4245a4d8a9dba0
Chris Hall <31258080+chall168421@users.noreply.github.com>
2021-12-21 15:21:27 +0700

    day 16 part a - example tests passed

--- day16a.py
+++ day16a.py
@@ -7,152 +7,85 @@ PP_ARGS = False, False #rotate, cast int
 DAY = 16
 TEST_DELIM = "---"
 FILE_DELIM = "\n"
-TESTS = """D2FE28///6
+TESTS = """EE00D40C823060///14
 ---
-38006F45291200///9
----
-EE00D40C823060///14
+620080001611562C8802118E34///12
 ---
 8A004A801A8002F478///16
 ---
 C0015000016115A2E0802F182340///23
 ---
-620080001611562C8802118E34///12
----
 A0016C880162017C3686B18A3D4780///31
-
----"""
+"""
 
 DEBUG = True
 
-#0123456789012345678901234567890123456789
-#00111000000000000110111101000101001010010001001000000000
-#VVVTTTILLLLLLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBBBBBB
+def pop_bits(bits, amt):
+    return [bits.pop(0) for i in range(amt)]
 
+def get_bits(hexa):
+    return list("".join(bin(int(d, 16))[2:].zfill(4) for d in hexa))
 
+def get_version(bits):
+    version = pop_bits(bits, 3)
+    version_num = int("".join(version), 2)
+    print(f"The three bits labeled V ({version}) are the packet version, {version_num}.")
+    return version_num, bits
 
-def process_literal(bits, version, tot):
-    print("LITERAL", bits)
+def get_type_id(bits):
+    type_ = pop_bits(bits, 3)
+    type_id = int("".join(type_), 2)
+    print(f"The three bits labeled V ({type_}) are the packet type_id, {type_id}.")
+    return type_id, bits
 
-    
-    
-    try:
-        number = ""
-        chunk_num = 0
-        while True:
-            print("chunking", bits)
-            chunk = "".join(bits.pop(0) for i in range(5))
-            number += chunk[1:]
-            if chunk[0] == "0":
-                break
-            chunk_num += 1
-        print(f"{number} literal was {int(number, 2)}")
-
-    except:
-        pass
-
-    
-    return tot, bits
-
-
-
-def process_operator(bits, version, tot):
-
-    print(bits)
-    final_bits = None
-    length_type_id = int(bits.pop(0), 2)
-    print(f"{length_type_id} length_type_id is {length_type_id}")
-
-
-    if length_type_id == 0:
-        length = int("".join(bits.pop(0) for i in range(15)), 2)
-        print("length is", length)
-        bits = bits[:length]
-        
-        print(f"sub_packet is now {''.join(bits)}")
-        tot, bits = process_bits(bits, tot)
-        
+def get_length(bits):
+    length_type = pop_bits(bits, 1)[0]
+    exp_bits = {'0':15, '1':11}[length_type]
+    length = int("".join(pop_bits(bits, exp_bits)), 2)
+    print(f"The length type ID is {length_type}")
+    if length_type == '0':
+        print(f"the next 15 bits are a number that represents the total length in bits of the sub-packets contained by this packet.")
     else:
-        number_of_sub_packets = "".join(bits.pop(0) for i in range(11))
-        
-        print(f"{number_of_sub_packets} num of sub packets is {int(number_of_sub_packets, 2)}", )
-        number_of_sub_packets = int(number_of_sub_packets, 2)
+        print(f"the next 11 bits are a number that represents the number of sub-packets immediately contained by this packet.")
+    return length, length_type, bits
 
-        bits_left = len("".join(bits).rstrip("0"))
+def get_literal(bits):
+    literal = []
+    end_literal = False
+    while not end_literal:
+        chunk = pop_bits(bits, 5)
+        literal += chunk[1:]
+        end_literal = chunk[0] == "0"
+        print(f"The five bits ({chunk}) start with a {chunk[0]} contain four bits of the number, {chunk[1:]}.")
 
-        bits_left = len(bits)
-        
-        packet_length = bits_left//number_of_sub_packets
-        
-        print(f"Packet length is {packet_length}")
-        for j in range(number_of_sub_packets):
-            
-            sub_packet = [bits.pop(0) for i in range(packet_length)]
-            print(f"sub_packet is now {(sub_packet)}")
-            
-            if not sub_packet:
-                break
-            tot, final_bits = process_bits(sub_packet, tot)
-
-        if final_bits:
-            bits = final_bits
-
-    print("Exit")
-        
-    return tot, bits
-
-    
-
-
-
-
-def process_bits(bits, tot=0):
-    print("".join(bits))
-
-    print(len(bits))
-    
-
-    while bits:
-        print(f"bits left {bits} {len(bits)} tot is {tot}")
-        version = "".join(bits.pop(0) for i in range(3))
-        
-        type_ = "".join(bits.pop(0) for i in range(3))
-
-        print(f"{version} version is {int(version, 2)}")
-        print(f"{type_} type is {int(type_, 2)}")
-
-        version, type_ = int(version, 2), int(type_, 2)
-
-        tot += version
-        
-        payload = bits
-
-        if type_ == 4:
-            tot, bits = process_literal(payload, version, tot)
-
-        else:
-            tot, bits = process_operator(payload, version, tot)
-
-    return tot, bits                  
-
-    
-        
-
-    
-    
+    literal_value = int("".join(literal), 2)
+    print(f"So, this packet represents a literal value with binary representation {literal}, which is {literal_value} in decimal.")
+    return literal_value, bits
 
 def solve(data):
     count = 0
-    tot = 0
 
-    bits = list("".join([bin(int(digit, 16))[2:].zfill(4) for digit in data[0]]).rstrip("0"))
+    bits = get_bits(data[0])
+    version_sum = 0
+    
+    while len(bits):
+        print("Bits left...", "".join(bits))
 
-    result = process_bits(bits)[0]
+        version, bits = get_version(bits)
+        version_sum += version
+        type_id, bits = get_type_id(bits)
+        
+        if type_id == 4:
+            literal, bits = get_literal(bits)
+        else:
+            length, length_type, bits = get_length(bits)
 
-    print(f"result is {result}")
 
-    return result
 
+        if set(bits) == {'0'}:
+            break
+
+    return version_sum
 
 
 
